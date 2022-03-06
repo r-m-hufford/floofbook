6e47c6ece06d34abc765120d049301728f29b7bb
Ryan Hufford <ryan@rmhufford.com>
2022-03-05 21:46:36 -0500

    added sign up route and link

--- app/components/login-form.js
+++ app/components/login-form.js
@@ -2,16 +2,15 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 
 export default class LoginFormComponent extends Component {
-
   /**
-   * 
-   * @param {Event & {target: HTMLFormEvent}} evt 
+   *
+   * @param {Event & {target: HTMLFormEvent}} evt
    */
   @action
-  onLoginFormSubmit(evt){
+  onLoginFormSubmit(evt) {
     evt.preventDefault();
-    console.log('submit button clicked')
+    console.log('submit button clicked');
     //@ts-ignore
     window.location = '/user';
   }
-};
+}
--- app/router.js
+++ app/router.js
@@ -9,4 +9,5 @@ export default class Router extends EmberRouter {
 Router.map(function () {
   this.route('login');
   this.route('user');
+  this.route('sign-up');
 });
